b24b73e66bd7af3cbcd5e105798d56d7e05e73fd
Sean Curtis <seanc@cs.unc.edu>
2015-10-22 21:10:12 -0400

    Initial commit
    Simple hello world.
    This uses a properties file to declare extra
    build environment macros (e.g., qt directory).

---
+++ MengeConfig/main.cpp
@@ -0,0 +1,12 @@
+#include <QtWidgets/qapplication.h>
+#include <QtWidgets/qpushbutton.h>
+
+int main( int argc, char **argv) {
+	QApplication a(argc, argv);
+	QPushButton hello( "Hello world!", 0);
+	hello.resize( 100, 30 );
+	a.setActiveWindow(&hello);
+	//a.setMainWidget( &hello);
+	hello.show();
+	return a.exec();
+}
